02c076ff971155c911f20c7ca7428b159152bc78
Mikki <mblackwell@georgiacyber.org>
2022-01-30 09:33:36 -0500

    added initial vars to js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,20 @@
+var saveBtnEl = document.getElementById('saveBtn');
+var currentDayEl = document.getElementById('lead');
+var hourEl = document.getElementById('hour');
+var userTextEl = document.getElementById('description');
+var timeBlockEl = document.getElementById('row');
+var timeTracker = 
 // Display today's day and date
 var todayDate = moment().format('dddd, MMM Do YYYY');
 $("#currentDay").html(todayDate);
+
+saveBtnEl.addEventListener('click', saveText);
+
+// save user input
+function saveText(){
+
+}
+
 
 // Color code time blocks for past, present, future
 // First, we need to track the current time before defining past, present, future
